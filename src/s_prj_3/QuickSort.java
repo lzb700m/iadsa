9f96b677ce67c5224639f0d94b14aad5fd364ea0
lzb700m <jakep.li@gmail.com>
2016-02-25 11:25:13 -0600

    implement dual pivot quick sort

--- src/s_prj_3/QuickSort.java
+++ src/s_prj_3/QuickSort.java
@@ -14,17 +14,27 @@ public class QuickSort {
 	private static final int INSERTION_SORT_CUTOFF = 17;
 
 	/**
-	 * public wrapper method for quick sort
+	 * public wrapper method for single pivot quick sort
 	 * 
 	 * @param A
 	 *            array to be sorted
 	 */
-	public static <T extends Comparable<? super T>> void quickSort(T[] A) {
-		quickSort(A, 0, A.length - 1);
+	public static <T extends Comparable<? super T>> void sQuickSort(T[] A) {
+		sQuickSort(A, 0, A.length - 1);
 	}
 
 	/**
-	 * Traditional quick-sort with one pivot implementation
+	 * public wrapper method for dual pivot quick sort
+	 * 
+	 * @param A
+	 *            array to be sorted
+	 */
+	public static <T extends Comparable<? super T>> void dQuickSort(T[] A) {
+		dQuickSort(A, 0, A.length - 1);
+	}
+
+	/**
+	 * Single pivot quick sort
 	 * 
 	 * @param A
 	 *            array to be sorted
@@ -33,19 +43,62 @@ public class QuickSort {
 	 * @param r
 	 *            end index of element in A to be sorted
 	 */
-	private static <T extends Comparable<? super T>> void quickSort(T[] A,
+	private static <T extends Comparable<? super T>> void sQuickSort(T[] A,
 			int p, int r) {
-		if (r - p >= INSERTION_SORT_CUTOFF) {
-			int q = partitionOne(A, p, r);
-			quickSort(A, p, q - 1);
-			quickSort(A, q + 1, r);
-		} else {
-			insertionSort(A, p, r);
+		if (r > p) {
+			int q = singlePivotPartition(A, p, r);
+			sQuickSort(A, p, q - 1);
+			sQuickSort(A, q + 1, r);
 		}
 	}
 
 	/**
-	 * Partition an array into 2 parts given one pivot
+	 * Dual pivot quick sort
+	 * 
+	 * @param A
+	 *            array to be sorted
+	 * @param p
+	 *            start index of element in A to be sorted
+	 * @param r
+	 *            end index of element in A to be sorted
+	 */
+	private static <T extends Comparable<? super T>> void dQuickSort(T[] A,
+			int p, int r) {
+		if (r > p) {
+			if (A[p].compareTo(A[r]) > 0) {
+				exchange(A, p, r);
+			}
+
+			int l = p + 1;
+			int g = r - 1;
+			int i = p + 1;
+
+			while (i <= g) {
+				if (A[i].compareTo(A[p]) < 0) {
+					exchange(A, l++, i++);
+				} else if (A[i].compareTo(A[r]) > 0) {
+					exchange(A, i, g--);
+				} else {
+					i++;
+				}
+			}
+
+			exchange(A, p, --l);
+			exchange(A, r, ++g);
+
+			dQuickSort(A, p, l - 1);
+
+			if (A[l].compareTo(A[g]) < 0) {
+				dQuickSort(A, l + 1, g - 1);
+			}
+
+			dQuickSort(A, g + 1, r);
+		}
+	}
+
+	/**
+	 * Partition an array into 2 parts using single pivot, should only be called
+	 * if r > p
 	 * 
 	 * @param A
 	 *            array to be partitioned
@@ -55,9 +108,9 @@ public class QuickSort {
 	 *            end index of element in A to be partitioned
 	 * @return index of pivot in A after partition
 	 */
-	private static <T extends Comparable<? super T>> int partitionOne(T[] A,
-			int p, int r) {
-		// int i = uniformRadom(p, r);
+	private static <T extends Comparable<? super T>> int singlePivotPartition(
+			T[] A, int p, int r) {
+		// precondition : r > p
 		int i = median3(A, p, r);
 		exchange(A, i, r); // move pivot to end
 
@@ -74,28 +127,12 @@ public class QuickSort {
 		return i;
 	}
 
-	/**
-	 * Generate a uniform random number from a given interval
-	 * 
-	 * @param min
-	 *            minimum value of the random number
-	 * @param max
-	 *            maximum value of the random number
-	 * @return
-	 */
 	private static int uniformRadom(int min, int max) {
 		Random rand = new Random();
 		int ret = rand.nextInt((max - min) + 1) + min;
 		return ret;
 	}
 
-	/**
-	 * 
-	 * @param A
-	 * @param p
-	 * @param r
-	 * @return
-	 */
 	private static <T extends Comparable<? super T>> int median3(T[] A, int p,
 			int r) {
 		int mid = (r - p) / 2 + p;
@@ -111,16 +148,6 @@ public class QuickSort {
 		return mid;
 	}
 
-	/**
-	 * Exchange 2 element in an array
-	 * 
-	 * @param A
-	 *            input array contains elements to be exchanged
-	 * @param m
-	 *            index of first element
-	 * @param n
-	 *            index of second element
-	 */
 	private static <T> void exchange(T[] A, int m, int n) {
 		T temp = A[m];
 		A[m] = A[n];
@@ -131,8 +158,11 @@ public class QuickSort {
 	 * Insertion sort
 	 * 
 	 * @param A
+	 *            array to be partitioned
 	 * @param p
+	 *            start index of element in A to be sorted
 	 * @param r
+	 *            end index of element in A to be sorted
 	 */
 	private static <T extends Comparable<? super T>> void insertionSort(T[] A,
 			int p, int r) {
@@ -146,6 +176,21 @@ public class QuickSort {
 			}
 			A[j + 1] = ai;
 		}
+	}
+
+	/**
+	 * 
+	 * @param A
+	 * @return
+	 */
+	private static <T extends Comparable<? super T>> boolean isSorted(T[] A) {
+		for (int i = 0; i < A.length - 2; i++) {
+			if (A[i].compareTo(A[i + 1]) > 0) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	/**
@@ -180,9 +225,18 @@ public class QuickSort {
 		 */
 		Integer[] input = list.toArray(new Integer[list.size()]);
 		timer.start();
-		quickSort(input);
+		sQuickSort(input);
 		timer.end();
+		System.out.println(isSorted(input));
+		System.out.println("Quick sort with single pivot:");
 		System.out.println(timer);
+
+		// timer.start();
+		// dQuickSort(input);
+		// timer.end();
+		// System.out.println(isSorted(input));
+		// System.out.println("Quick sort with dual pivot:");
+		// System.out.println(timer);
 
 		// Integer[] temp = new Integer[input.length];
 		// timer.start();
